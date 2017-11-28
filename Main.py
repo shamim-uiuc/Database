6dff9d36e9d9fda5712084689309860a7c291b26
Md Shamimuzzaman <33701852+shamim-uiuc@users.noreply.github.com>
2017-11-28 18:30:34 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
-import database_creation
-import insert_into_database_from_csv_with_header
-import read_from_database_write_to_file
+
+import Database_creation
+import Insert_from_CSV_file
+import Retrieve_data_from_database
